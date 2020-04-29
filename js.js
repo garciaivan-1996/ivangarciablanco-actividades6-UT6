8e8f1585b8c7bf2646b6848ba8c06cd5b6e4665d
garciaivan-1996 <60383188+garciaivan-1996@users.noreply.github.com>
2020-04-29 12:30:17 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,88 @@
+let color = String(prompt("Introduce el color con el que quieres que se muestre el texto rojo verde o azul"));
+let cadena = String(prompt("Introduce tu nombre"));
+let numero = String(prompt("Introduce un numero de telefono"));
+let correo = String(prompt("Introduce un Email"));
+let numero2=0;
+let freno = 1;
+let freno2 = 1;
+let freno3 = 1;
+
+
+function cambiaC(color){
+	
+
+
+	if(color == "verde"){
+	var cambia = document.querySelector("#cambia");
+	cambia.style.color = 'green';
+	
+	} else {
+		if(color == "rojo"){
+			var cambia = document.querySelector("#cambia");
+			cambia.style.color = 'red';
+		}else{
+			if(color == "azul"){
+				var cambia = document.querySelector("#cambia");
+				cambia.style.color = 'blue';
+			}else{
+				var cambia = document.querySelector("#cambia");
+				cambia.style.color = 'black';
+			}
+		}
+	}
+
+}
+
+
+function nombre(cadena){
+
+
+	while (freno == 1){
+		cadena2 = cadena.slice(0,1);
+		if(cadena2.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)){
+		document.querySelector("#nombre").textContent = cadena;
+		freno = 0;
+			
+		}else{
+		
+		cadena = String(prompt("Introduce tu nombre valido, debe empezar por mayusculas"));
+		
+		}		
+	}
+		
+}
+
+function telefono(numero){
+
+
+	while(freno2 == 1){
+	numero2 = numero.length
+		if(numero2 == 9){
+		document.querySelector("#telefono").textContent = numero;
+		freno2 =0;
+		}else{
+			numero = String(prompt("Introduce un numero de telefono valido"));
+		}
+	}
+}
+
+
+
+function email(correo) {
+ 
+	while(freno3 == 1){	
+		if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(correo)){
+		   document.querySelector("#correo").textContent = correo;
+		   freno3 = 0;
+		  } else {
+		  correo = String(prompt("Introduce un Email valido"));
+		  }
+	}
+}
+
+email(correo);
+telefono(numero);
+nombre(cadena);
+cambiaC(color);
+
+
